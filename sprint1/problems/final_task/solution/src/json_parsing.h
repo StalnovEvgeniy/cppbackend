86cad0ef938a4609bcc170ba90cb314be971de52
Евгений Стальнов <st-evgeniy@yandex.ru>
2024-02-24 00:22:06 +0300

    Create Dockerfile

---
+++ sprint1/problems/final_task/solution/src/json_parsing.h
@@ -0,0 +1,21 @@
+#ifndef JSON_PARSING_H
+#define JSON_PARSING_H
+
+#pragma once
+
+#include <boost/json.hpp>
+
+#include "model.h"
+
+namespace json_parsing {
+
+using namespace model;
+
+boost::json::value GetJsonDataError(std::string_view code, std::string_view message);
+boost::json::value GetJsonData(const Game::Maps& maps);
+boost::json::value GetJsonData(const Map& map);
+
+}  // namespace json_serializer
+
+
+#endif // JSON_PARSING_H
